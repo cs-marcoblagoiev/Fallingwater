13f0f42224dc775fe160f5f6fcfb298d485ab8ac
Marco <marcoblagoiev@yahoo.com>
2018-01-05 16:31:27 +0200

    modified all the doors to open as the character aproaches

---
+++ Fallingwater Project/Assets/Animation/DoorTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour {
+
+	private Animator DoorAnim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DoorAnim = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter () {
+		DoorAnim.SetBool ("RunAnimation", true);
+	}
+
+	void OnTriggerExit () {
+		DoorAnim.SetBool ("RunAnimation", false);
+	}
+}
